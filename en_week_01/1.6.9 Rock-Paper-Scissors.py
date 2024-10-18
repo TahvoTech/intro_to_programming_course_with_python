c6bda3f8457d1245dcf7edf3188beefd94140fd9
DataTinkerer / DataNikkari <jarmo.tahvanainen@gmail.com>
2024-10-19 00:38:28 +0300

    added task 1.6.9

---
+++ en_week_01/1.6.9 Rock-Paper-Scissors.py
@@ -0,0 +1,53 @@
+# COMP.CS.100
+# Tekijä: Jarmo Tahvanainen
+# Opiskelijanumero: 151737413
+
+def main():
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+'''
+Rock-Paper-Scissors
+
+Learning Goals
+
+Learning to create a somewhat more complicated if structure.
+
+Lets create a game of rock-paper-scissors for two players. In this game, both players use one letter to tell whether they choose rock (R), paper (P) or scissors (S). After this, the program shows the result. Paper beats rock, rock beats scissors and scissors beat paper.
+
+Examples of how the program operates:
+
+Player 1, enter your choice (R/P/S): P
+Player 2, enter your choice (R/P/S): S
+Player 2 won!
+
+Player 1, enter your choice (R/P/S): P
+Player 2, enter your choice (R/P/S): R
+Player 1 won!
+
+Player 1, enter your choice (R/P/S): R
+Player 2, enter your choice (R/P/S): S
+Player 1 won!
+
+Player 1, enter your choice (R/P/S): S
+Player 2, enter your choice (R/P/S): S
+It's a tie!
+
+The program does not need to consider incorrect entries, so there is no need to verify if the user entered something other than the letters "R"`, "P" or "S".
+
+Programming tips:
+
+    The program’s if structure can take a large variety of forms. A good way to start is recognizing the situations that rule each other out. This means that if player 1 wins, player 2 cannot win and there cannot be a tie. This knowledge can be utilized in designing the if structure.
+
+    String type variables (str) where introduced shortly with data types. Using of string variables is very similar to the use of integer variables. Remember that the Python program code, a string that contains the letter P is marked "P". Instead, just entering P without quotation marks refers to a variable named P. If you thus wish to create a variable named answer, which has the string "P" as its value, enter the following in Python: answer = "P"
+
+Remember to neatly distribute long lines on several code lines!
+
+'''
