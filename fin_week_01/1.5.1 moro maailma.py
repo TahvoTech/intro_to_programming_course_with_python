9818a2474239247ceb216bc5a1f0ba506b4c0acc
DataTinkerer / DataNikkari <jarmo.tahvanainen@gmail.com>
2024-10-18 21:07:56 +0300

    added folders

---
+++ fin_week_01/1.5.1 moro maailma.py
@@ -0,0 +1,5 @@
+# COMP.CS.100 1.5.1 moro maailma.
+# Tekijä: Jarmo Tahvanainen
+# Opiskelijanumero: 151737413
+
+print("Hello World!")
---
+++ fin_week_01/1.6.3 Tervehdys.py
@@ -0,0 +1,7 @@
+# COMP.CS.100
+# Tekijä: Jarmo Tahvanainen
+# Opiskelijanumero: 151737413
+
+name = input("Tell your name: ")
+print(f'Hi {name}')
+print("your coding skills are great!")
