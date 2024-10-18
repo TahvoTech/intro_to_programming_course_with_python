a3ad3a7d55ce6b22b74dd9fc2173f5bc5cec07ae
DataTinkerer / DataNikkari <jarmo.tahvanainen@gmail.com>
2024-10-18 23:48:22 +0300

    finished task 1.6.6

--- en_week_01/1.6.6 Greeting Using the Main Function.py
+++ en_week_01/1.6.6 Greeting Using the Main Function.py
@@ -1,3 +1,13 @@
 # COMP.CS.100
 # Tekijä: Jarmo Tahvanainen
 # Opiskelijanumero: 151737413
+
+
+def main():
+
+    name = input("Tell your name: ")
+    print(f'Hi {name}')
+    print("your coding skills are great!")
+
+if __name__ == "__main__":
+    main()
