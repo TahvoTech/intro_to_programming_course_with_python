5ff0ea35380d146407ec85463540867b6cea3d9b
DataTinkerer / DataNikkari <jarmo.tahvanainen@gmail.com>
2024-10-28 10:41:51 +0200

    added task 2.3.2

---
+++ en_week_02/2.3.2 Multiplication table - values over a hundred.py
@@ -0,0 +1,43 @@
+"""
+COMP.CS.100 Programming 1
+developer: Jarmo Tahvanainen
+email: jarmo.tahvanainen@gmail.com
+student id: 151737413
+"""
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
+
+'''
+
+Task:
+
+Multiplication table, values over a hundred
+
+Learning Goals
+
+Creating a repetition structure where the number of repetitions is not known (calculated) in advance. Comparing the repetition structure to the repetition structure implemented in the previous part.
+
+Create a program that prints a multiplication table for a given number until it reaches a result that is more than hundred.
+
+Example:
+
+Choose a number: 6
+1 * 6 = 6
+2 * 6 = 12
+3 * 6 = 18
+...
+17 * 6 = 102
+
+Programming tips:
+
+    One possible way is calculating the number of needed multiplications at the start of the program. Apart from the calculation, this does not differ too much from the implementation of the program in the previous part and thus teaches nothing new.
+
+    Another possibility, more interesting for learning purposes, is to re-think the type of the loop structure (while or for) and the condition of the loop structure. What needs to be checked on every repetition?
+
+
+'''
